5e1c4d110aa3efdecdd5737abeb23eb453f9b796
amit <sutharamit707@gmail.com>
2023-12-07 19:20:12 +0530

    db-setup

--- app/page.tsx
+++ app/page.tsx
@@ -23,14 +23,14 @@ export default function Page() {
             , brought to you by Vercel.
           </p>
           <Link
-            href="/login"
+            href="/dashboard"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+            <span>Check the development progress yet</span>
+            <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
           <Image
             src={'/hero-desktop.png'}
             width={1000}
@@ -42,7 +42,7 @@ export default function Page() {
             src={'/hero-mobile.png'}
             width={560}
             height={620}
-            className='block md:hidden'
+            className="block md:hidden"
             alt="Screenshots of the dashboard project showing mobile version"
           />
         </div>
